b182aaac2b6d8496ac0e1e50728ec99855c80c32
Wojtas399 <wojtekp26@gmail.com>
2022-09-17 19:45:39 +0200

    Deleted style from button component.

--- lib/components/buttons/button.dart
+++ lib/components/buttons/button.dart
@@ -12,15 +12,11 @@ class Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color disabledColor = Theme.of(context).brightness == Brightness.dark
-        ? Colors.white
-        : Colors.black;
     return SizedBox(
       width: 320,
       height: 51,
       child: ElevatedButton(
         onPressed: onPressed,
-        style: ElevatedButton.styleFrom(onSurface: disabledColor),
         child: Text(
           label.toUpperCase(),
           style: const TextStyle(color: Colors.white, fontSize: 14),
